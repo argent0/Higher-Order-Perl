558cda62f6ed91efdde78927034787ce300ba6b6
Aner Lucero <4rgento@gmail.com>
2012-02-26 17:23:29 -0300

    Stream utilities finished

--- Functional/Stream.pm
+++ Functional/Stream.pm
@@ -39,4 +39,40 @@ sub upto_list {
 	return if $m>$n;
 	return node($m, promise { upto_list($m+1,$n) });
 }
+
+sub upfrom {
+	my $n = $_[0];
+	return node($n, promise { upfrom($n+1) });
+}
+
+sub show {
+	my ($s,$n) = @_;
+	while( $s && (!defined $n ||  $n-- > 0 ) ) {
+		print drop($s),$";
+		#$s = tail($s);
+	}
+	print $/;
+}
+
+sub drop {
+	my $h = head($_[0]);
+	$_[0] = tail($_[0]);
+	return $h;
+}
+
+sub transform (&$) {
+	my ($f,$s) = @_;
+	return unless $s;
+	node($f->(head($s)),
+		promise { transform( $f,tail($s)) });
+}
+
+sub filter (&$) {
+	my ($f,$s) = @_;
+	until( !$s || $f->(head($s))) {
+		drop($s);
+	}
+	return if !$s;
+	node(head($s), promise { filter($f,tail($s))});
+}
 1;
